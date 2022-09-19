a15cb0caa7e9ae8a197ac88d2defc3829370435e
takaosecond <takaosecond@icloud.com>
2022-09-19 10:06:27 +0900

    first-commit

---
+++ main.js
@@ -0,0 +1,33 @@
+let imagesItems = [...document.querySelectorAll('.img-wrap')];
+let titles = [...document.querySelectorAll('h2')];
+let titleMessages = document.querySelector('.title');
+
+let setItemActive = (entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("active");
+        } else {
+            entry.target.classList.remove("active");
+        }
+    });
+};
+
+let options = {
+    rootMargin: "0px",
+    threshold: 0.5,
+};
+
+let observer = new IntersectionObserver(setItemActive, options);
+
+observer.observe(titleMessages)
+
+imagesItems.map((item, index) => {
+    item.children[0].style.backgroundImage = `url(./images/${index + 1}.jpg)`;
+    index % 2 == 0 ? (item.style.left = "55%") : (item.style.left = "5%");
+    observer.observe(item);
+})
+
+titles.map((title, index) => {
+    index % 2 == 0 ? (title.style.left = "45%") : (title.style.left = "35%");
+    observer.observe(title);
+})
